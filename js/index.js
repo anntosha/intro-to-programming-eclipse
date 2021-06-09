c8aa9959d765cb1df652a8286dc7604764afdf40
anntosha <annaantval@gmail.com>
2021-06-08 21:45:49 -0500

    Adding contact form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Copiright sign and skills list
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
@@ -14,3 +15,64 @@ for (let i = 0; i < skills.length; i++) {
 
 copiright.innerHTML = `Hanna Val ${thisYear}`;
 footer.appendChild(copiright);
+
+//Message form
+const messageForm = document.getElementsByName('leave_message');
+const messageSection = document.getElementById('messages');
+messageSection.style.display = 'none';
+
+messageForm[0].addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Geting form values
+    const inputElements = document.querySelectorAll('input, textarea');
+    const nameField = inputElements[0].value;
+    const emailField = inputElements[1].value;
+    const messageField = inputElements[2].value;
+    console.log(nameField, emailField, messageField);
+
+    //Making messages
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    const messageLine = `<a href="mailto:${emailField}">${nameField}</a> wrote:
+    <span>${messageField} </span>`;
+    newMessage.innerHTML = messageLine;
+    
+    //Creating remove button
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', () => {
+        const entry = removeButton.parentNode.parentNode;
+        entry.removeChild(newMessage);
+
+    //Message section reset if removing all the messages
+        if (messageList.children.length < 1) {
+            messageSection.style.display = 'none';
+        }
+    });
+
+    //Creating edit button
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    editButton.type = 'button';
+    editButton.addEventListener('click', () => {
+        const correctInput = document.createElement('input');
+        correctInput.type = 'text';
+        correctInput.value = messageField;
+        newMessage.appendChild(correctInput);
+        editButton.textContent = 'save';
+    });
+
+    messageList.appendChild(newMessage); 
+    newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);    
+    
+    //Display message section if someone live message
+    if (messageList.children.length > 0) {
+        messageSection.style.display = 'block';
+    }
+
+    messageForm[0].reset();
+});
+
